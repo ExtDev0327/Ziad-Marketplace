a0d323e113d0ece5ac830fb26d86a622cb0975b5
extdev0327 <akirasaito0327@gmail.com>
2022-04-21 06:46:46 +0900

    feat: unstoppable auction

---
+++ contracts/mocks/ERC721Test.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract ERC721Test is ERC721 {
+    constructor(string memory name_, string memory symbol_)
+        ERC721(name_, symbol_)
+    {}
+
+    function mint(address to, uint256 tokenId) external {
+        _mint(to, tokenId);
+    }
+}
